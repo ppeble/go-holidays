2465beffaed660f6363134e77c9003e9b9a988f4
Phil Peble <phil@peble.net>
2026-05-13 04:15:56 -0500

    port US custom methods and generate definitions for 59 regions
    Signed-off-by: Phil Peble <phil@peble.net>

---
+++ pkg/engine/methods_us.go
@@ -0,0 +1,94 @@
+package engine
+
+import (
+	"time"
+
+	"github.com/ppeble/go-holidays/pkg/calc"
+)
+
+func init() {
+	RegisterMethod("christmas_eve_holiday", func(a MethodArgs) (time.Time, error) {
+		switch a.Date.Weekday() {
+		case time.Saturday:
+			return a.Date.AddDate(0, 0, -1), nil
+		case time.Sunday:
+			return a.Date.AddDate(0, 0, -2), nil
+		}
+		return a.Date, nil
+	})
+
+	RegisterMethod("rosh_hashanah", func(a MethodArgs) (time.Time, error) {
+		dates := map[int]time.Time{
+			2014: time.Date(2014, 9, 25, 0, 0, 0, 0, time.UTC),
+			2015: time.Date(2015, 9, 14, 0, 0, 0, 0, time.UTC),
+			2016: time.Date(2016, 10, 3, 0, 0, 0, 0, time.UTC),
+			2017: time.Date(2017, 9, 21, 0, 0, 0, 0, time.UTC),
+			2018: time.Date(2018, 9, 10, 0, 0, 0, 0, time.UTC),
+			2019: time.Date(2019, 9, 30, 0, 0, 0, 0, time.UTC),
+			2020: time.Date(2020, 9, 19, 0, 0, 0, 0, time.UTC),
+		}
+		return dates[a.Year], nil
+	})
+
+	RegisterMethod("yom_kippur", func(a MethodArgs) (time.Time, error) {
+		dates := map[int]time.Time{
+			2014: time.Date(2014, 10, 4, 0, 0, 0, 0, time.UTC),
+			2015: time.Date(2015, 9, 23, 0, 0, 0, 0, time.UTC),
+			2016: time.Date(2016, 10, 12, 0, 0, 0, 0, time.UTC),
+			2017: time.Date(2017, 9, 30, 0, 0, 0, 0, time.UTC),
+			2018: time.Date(2018, 9, 19, 0, 0, 0, 0, time.UTC),
+			2019: time.Date(2019, 10, 9, 0, 0, 0, 0, time.UTC),
+			2020: time.Date(2020, 9, 28, 0, 0, 0, 0, time.UTC),
+		}
+		return dates[a.Year], nil
+	})
+
+	// Georgia State Holiday: Monday on or before April 26.
+	RegisterMethod("georgia_state_holiday", func(a MethodArgs) (time.Time, error) {
+		d := time.Date(a.Year, time.Month(a.Month), 26, 0, 0, 0, 0, time.UTC)
+		for d.Weekday() != time.Monday {
+			d = d.AddDate(0, 0, -1)
+		}
+		return d, nil
+	})
+
+	// Lee-Jackson Day: Friday before the third Monday in January.
+	RegisterMethod("lee_jackson_day", func(a MethodArgs) (time.Time, error) {
+		thirdMonday := calc.DayOfMonth(a.Year, time.Month(a.Month), 3, time.Monday)
+		d := thirdMonday
+		for d.Weekday() != time.Friday {
+			d = d.AddDate(0, 0, -1)
+		}
+		return d, nil
+	})
+
+	// Election Day: Tuesday after the first Monday of November.
+	RegisterMethod("election_day", func(a MethodArgs) (time.Time, error) {
+		firstMonday := calc.DayOfMonth(a.Year, time.November, 1, time.Monday)
+		return firstMonday.AddDate(0, 0, 1), nil
+	})
+
+	// Same as election_day but only in even years.
+	RegisterMethod("even_year_election_day", func(a MethodArgs) (time.Time, error) {
+		if a.Year%2 != 0 {
+			return time.Time{}, nil
+		}
+		firstMonday := calc.DayOfMonth(a.Year, time.November, 1, time.Monday)
+		return firstMonday.AddDate(0, 0, 1), nil
+	})
+
+	// Inauguration Day: January 20 in years following a presidential election
+	// (i.e. year % 4 == 1: 2017, 2021, 2025, ...).
+	RegisterMethod("us_inauguration_day", func(a MethodArgs) (time.Time, error) {
+		if a.Year%4 != 1 {
+			return time.Time{}, nil
+		}
+		return time.Date(a.Year, time.January, 20, 0, 0, 0, 0, time.UTC), nil
+	})
+
+	// Day after Thanksgiving: day after the 4th Thursday of November.
+	RegisterMethod("day_after_thanksgiving", func(a MethodArgs) (time.Time, error) {
+		thanksgiving := calc.DayOfMonth(a.Year, time.November, 4, time.Thursday)
+		return thanksgiving.AddDate(0, 0, 1), nil
+	})
+}
