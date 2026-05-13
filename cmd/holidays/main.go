759896532858f138835e8067153eef6b9929379a
Phil Peble <phil@peble.net>
2026-05-13 04:16:00 -0500

    add holidays CLI with on/between/year/regions subcommands
    Signed-off-by: Phil Peble <phil@peble.net>

---
+++ cmd/holidays/main.go
@@ -0,0 +1,203 @@
+// holidays is a small command-line wrapper around the go-holidays library.
+//
+// Subcommands:
+//
+//	holidays on DATE       [--regions r1,r2] [--informal] [--observed]
+//	holidays between A B   [--regions r1,r2] [--informal] [--observed]
+//	holidays year YYYY     [--regions r1,r2] [--informal] [--observed]
+//	holidays regions
+//
+// Dates are YYYY-M-D (single- or double-digit month/day both accepted).
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+	"time"
+
+	holidays "github.com/ppeble/go-holidays/pkg"
+	_ "github.com/ppeble/go-holidays/pkg/definitions"
+)
+
+func main() {
+	if err := run(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, "holidays:", err)
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
+	if len(args) == 0 {
+		printUsage()
+		return fmt.Errorf("missing subcommand")
+	}
+	cmd, rest := args[0], args[1:]
+	switch cmd {
+	case "on":
+		return cmdOn(rest)
+	case "between":
+		return cmdBetween(rest)
+	case "year":
+		return cmdYear(rest)
+	case "regions":
+		return cmdRegions(rest)
+	case "-h", "--help", "help":
+		printUsage()
+		return nil
+	default:
+		printUsage()
+		return fmt.Errorf("unknown subcommand %q", cmd)
+	}
+}
+
+func printUsage() {
+	fmt.Fprintln(os.Stderr, `usage:
+  holidays on DATE       [--regions r1,r2] [--informal] [--observed]
+  holidays between A B   [--regions r1,r2] [--informal] [--observed]
+  holidays year YYYY     [--regions r1,r2] [--informal] [--observed]
+  holidays regions`)
+}
+
+func cmdOn(args []string) error {
+	fs := newFlags("on")
+	regions, informal, observed := bindCommonFlags(fs)
+	if err := parseLeading(fs, args, 1); err != nil {
+		return err
+	}
+	d, err := parseDate(fs.Arg(0))
+	if err != nil {
+		return err
+	}
+	hs, err := holidays.On(d, optionsFrom(regions, informal, observed))
+	if err != nil {
+		return err
+	}
+	return printHolidays(hs)
+}
+
+func cmdBetween(args []string) error {
+	fs := newFlags("between")
+	regions, informal, observed := bindCommonFlags(fs)
+	if err := parseLeading(fs, args, 2); err != nil {
+		return err
+	}
+	start, err := parseDate(fs.Arg(0))
+	if err != nil {
+		return err
+	}
+	end, err := parseDate(fs.Arg(1))
+	if err != nil {
+		return err
+	}
+	hs, err := holidays.Between(start, end, optionsFrom(regions, informal, observed))
+	if err != nil {
+		return err
+	}
+	return printHolidays(hs)
+}
+
+func cmdYear(args []string) error {
+	fs := newFlags("year")
+	regions, informal, observed := bindCommonFlags(fs)
+	if err := parseLeading(fs, args, 1); err != nil {
+		return err
+	}
+	year, err := strconv.Atoi(fs.Arg(0))
+	if err != nil {
+		return fmt.Errorf("year: %w", err)
+	}
+	hs, err := holidays.YearHolidays(year, optionsFrom(regions, informal, observed))
+	if err != nil {
+		return err
+	}
+	return printHolidays(hs)
+}
+
+func cmdRegions(args []string) error {
+	for _, r := range holidays.AvailableRegions() {
+		fmt.Println(r)
+	}
+	return nil
+}
+
+func newFlags(name string) *flag.FlagSet {
+	return flag.NewFlagSet(name, flag.ContinueOnError)
+}
+
+func bindCommonFlags(fs *flag.FlagSet) (*string, *bool, *bool) {
+	regions := fs.String("regions", "", "comma-separated region codes (empty == all)")
+	informal := fs.Bool("informal", false, "include informal holidays")
+	observed := fs.Bool("observed", false, "use observed dates")
+	return regions, informal, observed
+}
+
+func parseLeading(fs *flag.FlagSet, args []string, n int) error {
+	if err := fs.Parse(reorderFlagsFirst(args)); err != nil {
+		return err
+	}
+	if fs.NArg() < n {
+		return fmt.Errorf("%s expects %d positional argument(s)", fs.Name(), n)
+	}
+	return nil
+}
+
+// reorderFlagsFirst lets users put flags after positional args (e.g.
+// `year 2024 --regions us`) by moving flags to the front before flag.Parse.
+var boolFlagNames = map[string]bool{"informal": true, "observed": true}
+
+func reorderFlagsFirst(args []string) []string {
+	var flags, positional []string
+	for i := 0; i < len(args); i++ {
+		a := args[i]
+		if !strings.HasPrefix(a, "-") {
+			positional = append(positional, a)
+			continue
+		}
+		flags = append(flags, a)
+		if strings.Contains(a, "=") {
+			continue
+		}
+		name := strings.TrimLeft(a, "-")
+		if boolFlagNames[name] {
+			continue
+		}
+		if i+1 < len(args) {
+			flags = append(flags, args[i+1])
+			i++
+		}
+	}
+	return append(flags, positional...)
+}
+
+func optionsFrom(regions *string, informal, observed *bool) holidays.Options {
+	opts := holidays.Options{Informal: *informal, Observed: *observed}
+	if r := strings.TrimSpace(*regions); r != "" {
+		for _, s := range strings.Split(r, ",") {
+			if s = strings.TrimSpace(s); s != "" {
+				opts.Regions = append(opts.Regions, s)
+			}
+		}
+	}
+	return opts
+}
+
+func parseDate(s string) (time.Time, error) {
+	return time.Parse("2006-1-2", s)
+}
+
+func printHolidays(hs []holidays.Holiday) error {
+	sort.Slice(hs, func(i, j int) bool {
+		if !hs[i].Date.Equal(hs[j].Date) {
+			return hs[i].Date.Before(hs[j].Date)
+		}
+		return hs[i].Name < hs[j].Name
+	})
+	for _, h := range hs {
+		fmt.Printf("%s\t%s\t%s\n", h.Date.Format("2006-01-02"), h.Name, strings.Join(h.Regions, ","))
+	}
+	return nil
+}
